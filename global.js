92432fc873d475a16b82eae053375e3d24c3df45
Andrew Watterson <awatterson@pivotal.io>
2019-03-14 22:49:41 -0700

    Added working webcam preview and rough design file.

--- global.js
+++ global.js
@@ -1,14 +1,27 @@
 window.addEventListener('load', async () => {
 
   let constraints = { audio: true, video: true };
-
   let videoPreview = document.querySelector('.webcam-preview');
 
   navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => {
-      console.log('got it!');
-      console.log(videoPreview);
+    .then(async (stream) => {
+
       videoPreview.srcObject = stream;
+
+      let recorder = RecordRTC(stream, {
+        type: 'video'
+      });
+
+      recorder.startRecording();
+
+      const sleep = m => new Promise(r => setTimeout(r, m));
+      await sleep(3000);
+
+      recorder.stopRecording(function() {
+          let blob = recorder.getBlob();
+          invokeSaveAsDialog(blob);
+      });
+
     })
     .catch((err) => {
       console.log('Could not activate the video camera.');
